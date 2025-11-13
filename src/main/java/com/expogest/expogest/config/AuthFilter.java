5034d05be433255eafe5e05a795411d22860bd13
Juan Duran <juandavidduranmalaver@gmail.com>
2025-11-12 22:26:26 -0500

    fix: Eliminar @Component de AuthFilter para evitar registro duplicado

--- src/main/java/com/expogest/expogest/config/AuthFilter.java
+++ src/main/java/com/expogest/expogest/config/AuthFilter.java
@@ -5,11 +5,9 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Component
 public class AuthFilter implements Filter {
 
     @Override
